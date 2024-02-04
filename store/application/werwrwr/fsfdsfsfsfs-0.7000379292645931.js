31476387f9328c97af1c38ecee21cc6b5a752cdb
Cui Bingbing <752856108@qq.com>
2024-02-04 16:14:34 +0800

    fix:tui全局滚动条

---
+++ store/application/werwrwr/fsfdsfsfsfs-0.7000379292645931.js
@@ -0,0 +1,13 @@
+
+    
+    customElements.define('fsfdsfsfsfs',
+      class MyComponent extends HTMLElement{
+        template = `<div id="grid-27059993875466250000" style="z-index: 10;position: relative;"><div id="grid-18897718616166625000-0" class="grid-item" style="text-align:left;align-items:normal;border-top:1px solid #00000017;border-right:1px solid #00000017;border-bottom:1px solid #00000017;border-left:1px solid #00000017;border-radius:0px;padding:0px;background-color:#fff0;background-image:url();background-repeat:no-repeat;background-size:cover;"></div></div>`;
+        constructor(){
+          super();
+          this.innerHTML = this.template;
+        }
+      }
+    );
+    
+    
